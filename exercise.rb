8feefefd7c6caf22c2e1f5aa4bf7b65d396afd0c
Sanderfer Chau <sanderfer.chau@gmail.com>
2016-11-30 10:52:34 -0800

    Add pseudocode for even_fibinacci

--- exercise.rb
+++ exercise.rb
@@ -3,6 +3,7 @@ class Exercise
   # Assume that "str" is a sequence of words separated by spaces.
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
+
   def self.marklar(str)
     # TODO: Implement this method
   end
@@ -11,6 +12,18 @@ class Exercise
   # the "nth" term in the sequence
   # eg. the Fibonacci sequence up to 6 terms is (1, 1, 2, 3, 5, 8),
   # and the sum of its even numbers is (2 + 8) = 10
+  
+  #Pseudocode
+    # Assumption: fibonacci sequence begins with 1 and 1
+    # create an fibonacci array and place starting values
+    # iterate through array n times
+    # sum elements in last position and last position - 1
+    # push sum into fibonacci array as the last element
+    # create an array to store all even numbers of the fibonacci sequence
+    # iterate through the fibonacci array and push all the even elements into the even array
+    # sum each number of the even array
+    # return the sum
+
   def self.even_fibonacci(nth)
     # TODO: Implement this method
   end
